5f82f95fdfc364a13ff750156dd392377d5182e3
Brandon <brandon.se2009@gmail.com>
2021-10-14 00:17:55 -0400

    Initial Commit

---
+++ Proyecto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Proyecto
+//
+//  Created by user204403 on 10/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Proyecto/ProyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoApp.swift
+//  Proyecto
+//
+//  Created by user204403 on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
